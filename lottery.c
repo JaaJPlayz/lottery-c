337636f145a790cf69fcc366805ed226a5f13603
JaaJPlayz <hiagoaugustofcunha@gmail.com>
2024-03-21 23:51:53 -0300

    Bro. . . I just made lottery in C (for the memes)

---
+++ lottery.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+  int cpu_numbers[6];
+  int user_numbers[6];
+
+  for (int i = 0; i < 6; i++) {
+    cpu_numbers[i] = rand() % 49 + 1;
+    for (int j = 0; j < i; j++) {
+      if (cpu_numbers[i] == cpu_numbers[j]) {
+        i--;
+        break;
+      }
+    }
+  }
+
+  for (int i = 0; i < 6; i++) {
+    printf("Enter number %d: ", i + 1);
+    scanf("%d", &user_numbers[i]);
+
+    if (user_numbers[i] < 1 || user_numbers[i] > 60) {
+      printf("Invalid number. Please enter a number between 1 and 60.\n");
+      i--;
+    }
+
+    for (int j = 0; j < i; j++) {
+      if (user_numbers[i] == user_numbers[j]) {
+        printf("Number already entered. Please enter a different number.\n");
+        i--;
+        break;
+      }
+    }
+  } 
+
+  printf("The lottery numbers are: ");
+  for (int i = 0; i < 6; i++) {
+    printf("%d ", cpu_numbers[i]);
+  }
+  printf("\n");
+
+  int count = 0;
+  for (int i = 0; i < 6; i++) {
+    for (int j = 0; j < 6; j++) {
+      if (user_numbers[i] == cpu_numbers[j]) {
+        count++;
+      }
+    }
+  }
+
+  printf("You guessed %d numbers correctly.\n", count);
+
+  if (count == 6) {
+    printf("Congratulations! You won the lottery!\n");
+  } else {
+    printf("Better luck next time.\n");
+  }
+
+  return 0;
+}
